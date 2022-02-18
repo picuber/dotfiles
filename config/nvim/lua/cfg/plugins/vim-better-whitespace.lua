d2f2928cbcd7d2bfa491b0fb6ad7116961fd056d
picuber <luna.fuchsloch@gmail.com>
2022-02-19 00:21:26 +0100

    [nvim] mooore plugins: emmet, whitespace, table-mode, ...

---
+++ config/nvim/lua/cfg/plugins/vim-better-whitespace.lua
@@ -0,0 +1,13 @@
+vim.g.strip_whitespace_on_save = 1
+vim.g.strip_whitespace_confirm = 0
+vim.g.better_whitespace_operator = ""
+vim.g.better_whitespace_filetypes_blacklist = {
+	"diff",
+	"git",
+	"gitcommit",
+	"unite",
+	"qf",
+	"help",
+	"markdown",
+	"fugitive",
+}
