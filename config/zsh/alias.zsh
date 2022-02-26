a6102a63e7c36b54765b648ea4de4907febcafe4
picuber <luna.fuchsloch@gmail.com>
2022-02-27 00:46:06 +0100

    [zsh] mv/cp ask before override

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -3,6 +3,10 @@ alias lh='ls -hlF --time-style=long-iso'
 alias la='ls -hAlF --time-style=long-iso'
 alias l='ls -CF'
 
+    # ask before override
+alias mv="mv -i"
+alias cp="cp -i"
+
 	# vim-like commands
 alias :q=exit
 alias :Q=exit
