4dae5971d973046b5a5a23734d3399a1eb60882c
picuber <luna.fuchsloch@gmail.com>
2023-03-27 20:48:34 +0200

    [nvim] luasnip all add date/time snippets

--- config/nvim/lua/cfg/plugins/luasnip/all.lua
+++ config/nvim/lua/cfg/plugins/luasnip/all.lua
@@ -1,10 +1,10 @@
 local ls = require("luasnip")
--- local s = ls.snippet
+local s = ls.snippet
 -- local sn = ls.snippet_node
 -- local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
 -- local d = ls.dynamic_node
 -- local r = ls.restore_node
 -- local l = require("luasnip.extras").lambda
@@ -18,4 +18,47 @@ local ls = require("luasnip")
 -- local types = require("luasnip.util.types")
 -- local conds = require("luasnip.extras.expand_conditions")
 
-ls.add_snippets("all", {}, { key = "all" })
+local function date(_, _, format)
+	return os.date(format)
+end
+
+ls.add_snippets("all", {
+	s({ trig = "date", name = "current date" }, {
+		f(date, {}, { user_args = { "%F" } }),
+	}),
+	s({ trig = "time", name = "current time" }, {
+		c(1, {
+			f(date, {}, { user_args = { "%R" } }),
+			f(date, {}, { user_args = { "%R %Z" } }),
+			f(date, {}, { user_args = { "%R%z" } }),
+		}),
+		i(0),
+	}),
+	s({ trig = "times", name = "current time w/ sec" }, {
+		c(1, {
+			f(date, {}, { user_args = { "%T" } }),
+			f(date, {}, { user_args = { "%T %Z" } }),
+			f(date, {}, { user_args = { "%T%z" } }),
+		}),
+		i(0),
+	}),
+	s({ trig = "dtime", name = "current date and time" }, {
+		c(1, {
+			f(date, {}, { user_args = { "%F %R" } }),
+			f(date, {}, { user_args = { "%F %R %Z" } }),
+			f(date, {}, { user_args = { "%F %R%z" } }),
+		}),
+		i(0),
+	}),
+	s({ trig = "dtimes", name = "current date and time w/ sec" }, {
+		c(1, {
+			f(date, {}, { user_args = { "%F %T" } }),
+			f(date, {}, { user_args = { "%F %T %Z" } }),
+			f(date, {}, { user_args = { "%F %T%z" } }),
+		}),
+		i(0),
+	}),
+	s({ trig = "epoch", name = "seconds since epoch" }, { f(function()
+		return os.date("%s")
+	end) }),
+}, { key = "all" })
