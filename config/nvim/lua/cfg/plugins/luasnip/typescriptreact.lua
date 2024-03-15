9cf7fcb08d34f8987123fc68ccc3617c347103b7
picuber <luna.fuchsloch@gmail.com>
2024-03-15 13:45:15 +0100

    [nvim] luasnip typescriptreact snippets

--- config/nvim/lua/cfg/plugins/luasnip/typescriptreact.lua
+++ config/nvim/lua/cfg/plugins/luasnip/typescriptreact.lua
@@ -1,9 +1,9 @@
 local ls = require("luasnip")
--- local s = ls.snippet
+local s = ls.snippet
 -- local sn = ls.snippet_node
--- local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
 -- local c = ls.choice_node
 -- local d = ls.dynamic_node
 -- local r = ls.restore_node
@@ -20,4 +20,42 @@ local ls = require("luasnip")
 
 ls.filetype_extend("typescriptreact", { "javascript" })
 
-ls.add_snippets("typescriptreact", {}, { key = "typescriptreact" })
+local function copy(idx)
+	return function(args)
+		return args[idx]
+	end
+end
+
+local function firstUp(func)
+	return function(args)
+        local value = func(args)[1]
+		return value:gsub("^%l", string.upper)
+	end
+end
+
+ls.add_snippets("typescriptreact", {
+	s({ trig = "com", name = "component" }, {
+		t("type "),
+		f(copy(1), 1),
+		t({ "Props = {};", "", "export const " }),
+		i(1, "ComponentName"),
+		t(" = ({}: "),
+		f(copy(1), 1),
+		t({ "Props) => {", "  " }),
+		i(0),
+		t({ "", "  return <></>;", "};" }),
+	}),
+
+	s({ trig = "uses", name = "useState" }, {
+		-- const [a, setA] = useState<>();
+		t("const ["),
+		i(1),
+		t(", set"),
+		f(firstUp(copy(1)), 1),
+		t("] = useState<"),
+		i(2),
+		t(">("),
+		i(0),
+		t(");"),
+	}),
+}, { key = "typescriptreact" })
