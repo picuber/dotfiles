389f4cb4bd218be923d23df8ccc11637c1a99c85
picuber <luna.fuchsloch@gmail.com>
2025-07-11 15:29:21 +0200

    [nvim] listchars

--- config/nvim/lua/cfg/settings.lua
+++ config/nvim/lua/cfg/settings.lua
@@ -3,19 +3,19 @@ vim.g.mapleader = ','
 vim.g.maplocalleader = ';'
 
 -- Languages --
-vim.g.tex_flavor = 'latex'  -- default to LaTeX
-vim.g.loaded_perl_provider=0 -- disable perl support
-vim.g.loaded_python_provider=0 -- disable python2 support
-vim.g.loaded_ruby_provider=0 -- disable ruby support
+vim.g.tex_flavor = 'latex'                                                -- default to LaTeX
+vim.g.loaded_perl_provider = 0                                            -- disable perl support
+vim.g.loaded_python_provider = 0                                          -- disable python2 support
+vim.g.loaded_ruby_provider = 0                                            -- disable ruby support
 vim.g.python3_host_prog = '~/.local/share/pyenv/versions/nvim/bin/python' -- set python3 executable
 
 -- greeter
-vim.opt.shortmess:append {I = true} -- disable intro message
+vim.opt.shortmess:append { I = true } -- disable intro message
 
 -- Cursor --
 -- vim.opt.cursorline = true
 -- vim.opt.cursorcolumn = true
-vim.opt.colorcolumn = {80, 120}
+vim.opt.colorcolumn = { 80, 120 }
 
 -- mouse --
 vim.opt.mouse = 'a' -- enable mouse in all modes
@@ -24,25 +24,37 @@ vim.opt.mouse = 'a' -- enable mouse in all modes
 vim.opt.title = true
 vim.opt.winborder = "rounded"
 
+local space = "·"
+vim.opt.listchars = {
+    eol = "⏎",
+    tab = ">-",
+    space = space,
+    multispace = space,
+    leadmultispace = string.rep(space, 3) .. "|",
+    nbsp = "⍽",
+    extends = "▶",
+    precedes = "◀"
+}
+
 -- updating --
 vim.opt.updatetime = 1000 -- update swaps more often (every 1000ms)
 vim.opt.lazyredraw = true -- don't redraw while executing macros/commands
 
 -- lines/scrolling --
-vim.opt.wrap = false -- don't wrap lines
-vim.opt.scrolloff = 3 -- start scrolling n lines before window border (horizontal)
-vim.opt.sidescrolloff = 7 -- start scrolling n lines before window border (vertical)
-vim.opt.whichwrap:append '<,>,[,]'  -- allow arrow keys to wrap
+vim.opt.wrap = false               -- don't wrap lines
+vim.opt.scrolloff = 3              -- start scrolling n lines before window border (horizontal)
+vim.opt.sidescrolloff = 7          -- start scrolling n lines before window border (vertical)
+vim.opt.whichwrap:append '<,>,[,]' -- allow arrow keys to wrap
 
 -- tabs --
-vim.opt.tabstop = 4 -- a tab is 4 spaces
-vim.opt.shiftwidth = 4 -- shift 4 spaces at autoindent
+vim.opt.tabstop = 4      -- a tab is 4 spaces
+vim.opt.shiftwidth = 4   -- shift 4 spaces at autoindent
 vim.opt.expandtab = true -- convert tabs to spaces
 
 -- search --
 vim.opt.ignorecase = true -- case-insensitive search ...
-vim.opt.smartcase = true -- ... except when using upper-case letters
-vim.opt.gdefault = true -- set 'g' flag to default on in :s, set flag to turn it off
+vim.opt.smartcase = true  -- ... except when using upper-case letters
+vim.opt.gdefault = true   -- set 'g' flag to default on in :s, set flag to turn it off
 
 -- command completion --
 vim.opt.wildignorecase = true -- file completion is case-insensitive
@@ -52,7 +64,7 @@ vim.opt.fixendofline = false
 
 -- file types --
 vim.filetype.add({
-	extension = {
-		rgple = "rpgle",
-	},
+    extension = {
+        rgple = "rpgle",
+    },
 })
