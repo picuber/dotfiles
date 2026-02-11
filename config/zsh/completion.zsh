6f37eb08f92b22cf7f0b996e5e859b79c93cffd3
picuber <luna.fuchsloch@gmail.com>
2026-02-11 15:24:35 +0100

    [zsh] add uv config

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -1,3 +1,6 @@
+uv generate-shell-completion zsh > ~/.config/zsh/completions/_uv
+uvx --generate-shell-completion zsh > ~/.config/zsh/completions/_uvx
+
 fpath=(~/.config/zsh/completions $fpath)
 
 autoload -U +X compinit && compinit # re-set here, because antigen breaks it
--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -16,6 +16,7 @@ antigen bundle tmux # tmux aliase
 # antigen bundle wd # shortcuts for directories
 antigen bundle rust # completions for rustc, cargo (rust build tool), rustup (rust toolchain manager)
 antigen bundle docker # completions for docker
+antigen bundle uv # completions and aliases for uv (python project-management)
 
 # antigen bundle zsh-users/zsh-syntax-highlighting # Syntax highlighting bundle.
 # antigen bundle zsh-users/zsh-autosuggestions # suggest recent matching commands from history
